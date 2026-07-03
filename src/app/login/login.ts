12cee6ed03f2bd24440affc601efffa8f65dfade
Abhishek Jayapal Huded <jhabhishek@ssdiblr.sharp.co.in>
2026-07-03 12:50:03 +0530

    login page updated

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,34 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  login()
+  {
+      const username ="admin";
+      const password ="admin123"
+
+      if(this.loginForm.value.username===username && 
+         this.loginForm.value.password===password
+      ){
+        alert("Login Successfull");
+      }else
+        {
+          alert("Invalid Username or Password");
+        }
+
+  }
+  loginForm = new FormGroup({
+  username: new FormControl('',[
+    Validators.required,
+    Validators.minLength(3)]),
+  password: new FormControl('',[Validators.required,Validators.minLength(5)])
+});
 
 }
